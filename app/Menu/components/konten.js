70d31e021359515c9858c0e5e9e463bc2c963bb5
Sayyid <sayyid.gibran212@gmail.com>
2025-02-12 04:28:36 +0700

    Finishing Menu

--- app/Menu/components/konten.js
+++ app/Menu/components/konten.js
@@ -11,116 +11,24 @@ import {
 // ICON
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { GoDot, GoDotFill } from "react-icons/go";
+//UTILS
+import { formatRupiah } from "@/utils/formatRupiah";
 // IMAGES
-import MMakanan from "@/assets/img/menu/menu1.png";
-import MMinuman1 from "@/assets/img/menu/menu2.png";
-import MMinuman2 from "@/assets/img/menu/menu3.png";
 import C1 from "@/assets/img/carousel/1.png";
 import C2 from "@/assets/img/carousel/2.png";
+const fotoMakanan = require("@/assets/img/menu/menu1.png");
+const fotoMinuman = require("@/assets/img/menu/menu2.png");
+// HOOKS
+import useTampilkanMakanan from "@/hooks/Backend/useTampilkanMakanan";
+import useTampilkanMinuman from "@/hooks/Backend/useTampilkanMinuman";
 
 function Beranda() {
   const scrollRef1 = React.useRef(null);
   const scrollRef2 = React.useRef(null);
   const scrollRef3 = React.useRef(null);
 
-  const menuMakanan = [
-    {
-      name: "Nasi Ayam Geprek",
-      description: "Sambal Matah",
-      price: "Rp 15.000",
-      image: MMakanan,
-    },
-    {
-      name: "Nasi Ayam Geprek",
-      description: "Sambal Bawang",
-      price: "Rp 20.000",
-      image: MMakanan,
-    },
-    {
-      name: "Nasi Ayam Geprek",
-      description: "Sambal Rica Manis",
-      price: "Rp 18.000",
-      image: MMakanan,
-    },
-    {
-      name: "Nasi Ayam Geprek",
-      description: "Sambal Rica Pedas",
-      price: "Rp 25.000",
-      image: MMakanan,
-    },
-    {
-      name: "Nasi Ayam Geprek",
-      description: "Sambal Kemangi",
-      price: "Rp 22.000",
-      image: MMakanan,
-    },
-  ];
-
-  const menuMinuman1 = [
-    {
-      name: "Kapal Api",
-      description: "Kopi",
-      price: "Rp 15.000",
-      image: MMinuman1,
-    },
-    {
-      name: "Kapal Api",
-      description: "Kopi",
-      price: "Rp 20.000",
-      image: MMinuman1,
-    },
-    {
-      name: "Kapal Api",
-      description: "Kopi",
-      price: "Rp 18.000",
-      image: MMinuman1,
-    },
-    {
-      name: "Kapal Api",
-      description: "Kopi",
-      price: "Rp 25.000",
-      image: MMinuman1,
-    },
-    {
-      name: "Kopi Kapal",
-      description: "Kopi",
-      price: "Rp 22.000",
-      image: MMinuman1,
-    },
-  ];
-
-  const menuMinuman2 = [
-    {
-      name: "Lemon Tea",
-      description: "Teh",
-      price: "Rp 15.000",
-      image: MMinuman2,
-    },
-    {
-      name: "Lemon Tea",
-      description: "Teh",
-      price: "Rp 20.000",
-      image: MMinuman2,
-    },
-    {
-      name: "Lemon Tea",
-      description: "Teh",
-      price: "Rp 18.000",
-      image: MMinuman2,
-    },
-    {
-      name: "Lemon Tea",
-      description: "Teh",
-      price: "Rp 25.000",
-      image: MMinuman2,
-    },
-    {
-      name: "Lemon Tea",
-      description: "Teh",
-      price: "Rp 22.000",
-      image: MMinuman2,
-    },
-  ];
+  const { daftarMakanan } = useTampilkanMakanan();
+  const { daftarMinuman } = useTampilkanMinuman();
 
   const scrollLeft1 = () => {
     if (scrollRef1.current) {
@@ -232,7 +140,7 @@ function Beranda() {
       </Carousel>
 
       <div className="w-full flex justify-center items-center mt-6 lg:mt-12">
-        <div className=" bg-[#FFF2C2] mx-5 lg:mx-0 px-2 py-8 lg:px-7 lg:py-10 rounded-lg flex flex-col justify-center items-center shadow-lg mb-9">
+        <div className="bg-[#FFF2C2] mx-3 px-2 py-8 sm:px-7 w-full lg:py-10 rounded-lg flex flex-col justify-center items-center shadow-lg mb-9">
           <div className="lg:flex hidden justify-center items-center gap-4 lg:gap-6 lg:mb-6">
             <Typography className="lg:px-7 lg:py-1 text-sm p-1 bg-[#AA5656] text-white border border-gray-500 tracking-wider cursor-pointer hover:rounded-3xl hover:scale-95 transform transition-all ease-in-out duration-300">
               Makanan
@@ -260,41 +168,45 @@ function Beranda() {
               Makanan
             </Typography>
           </div>
-          <div className="flex items-center w-full max-w-full lg:max-w-[90rem] lg:mb-12">
+          {/* 1 */}
+          <div className="flex items-center w-full max-w-full lg:max-w-[90rem] mb-6 lg:mb-9">
             <Button
               className="bg-black bg-opacity-25 p-2 hidden lg:flex justify-center items-center rounded-full hover:bg-opacity-15 hover:shadow-md hover:scale-105 transform transition-all ease-in-out duration-300"
               onClick={scrollLeft1}
             >
               <FaChevronLeft className="h-5 w-5 text-black" />
             </Button>
-
             <div
               ref={scrollRef1}
-              className="grid grid-cols-1 mb-6 lg:mb-0 lg:flex lg:flex-row gap-5 lg:gap-12 py-4 lg:overflow-x-scroll lg:scrollbar-none lg:scroll-smooth mx-4 max-w-full lg:max-w-[90%]"
+              className="flex flex-col sm:flex-row w-full gap-4 sm:gap-8 py-4 mx-4 sm:w-full"
             >
-              {menuMakanan.map((item, index) => (
+              {daftarMakanan.map((makanan) => (
                 <CardBody
-                  key={index}
-                  className="bg-[#EFF3EA] flex lg:flex-col gap-2 p-2 lg:gap-5 rounded-xl shadow-md lg:shadow-lg w-full lg:w-[250px] lg:flex-shrink-0"
+                  key={makanan.id}
+                  className="bg-[#EFF3EA] grid grid-cols-2 sm:flex sm:flex-col p-3 items-center sm:gap-0 gap-3 rounded-lg border-2 border-gray-300 shadow-md lg:shadow-lg w-full"
                 >
-                  <Image
-                    src={item.image}
-                    alt={`menu-${index + 1}`}
-                    className="w-1/2 lg:w-full lg:h-auto rounded-lg self-center"
-                  />
-                  <div className="flex-col ">
+                  <div className="w-full">
+                    <Image
+                      src={makanan.Gambar_Makanan || fotoMakanan}
+                      alt={`menu-${makanan.Nama_Makanan}`}
+                      width={400}
+                      height={400}
+                      className="w-full h-full object-cover rounded-lg"
+                    />
+                  </div>
+                  <div className="flex flex-col w-full flex-1">
                     <div className="py-3 space-y-1 lg:space-y-1">
-                      <Typography className="text-md lg:text-xl text-black">
-                        {item.name}
+                      <Typography className="text-lg lg:text-xl font-bold text-black">
+                        {makanan.Nama_Makanan}
                       </Typography>
-                      <Typography className="text-xs text-gray-600">
-                        {item.description}
-                      </Typography>
-                      <Typography className="text-md text-black">
-                        {item.price}
+                      <Typography className="text-md text-gray-600">
+                        {makanan.Deskripsi_Makanan}
                       </Typography>
                     </div>
-                    <div className="w-full lg:mt-2 flex justify-center items-center">
+                    <div className="w-full flex-col space-y-2 justify-center items-center mt-auto">
+                      <Typography className="text-lg text-black">
+                        {formatRupiah(makanan.Harga_Makanan)}
+                      </Typography>
                       <Button className="p-1 lg:py-2 w-full lg:rounded-full tracking-widest bg-[#AA5656] shadow-md hover:shadow-md">
                         beli
                       </Button>
@@ -303,7 +215,6 @@ function Beranda() {
                 </CardBody>
               ))}
             </div>
-
             <Button
               className="bg-black bg-opacity-25 p-2 hidden lg:flex justify-center items-center rounded-full hover:bg-opacity-15 hover:shadow-md hover:scale-105 transform transition-all ease-in-out duration-300"
               onClick={scrollRight1}
@@ -311,12 +222,13 @@ function Beranda() {
               <FaChevronRight className="h-5 w-5 text-black" />
             </Button>
           </div>
+          {/* 2 */}
           <div className="w-full">
             <Typography className="text-black text-start text-2xl lg:text-[36px] font-semibold mb-0 ml-3 lg:mb-5 lg:ml-10">
               Minuman (Coffe)
             </Typography>
           </div>
-          <div className="flex items-center w-full max-w-full lg:max-w-[90rem] lg:mb-9">
+          <div className="flex items-center w-full max-w-full lg:max-w-[90rem] mb-6 lg:mb-9">
             <Button
               className="bg-black bg-opacity-25 p-2 hidden lg:flex justify-center items-center rounded-full hover:bg-opacity-15 hover:shadow-md hover:scale-105 transform transition-all ease-in-out duration-300"
               onClick={scrollLeft2}
@@ -326,38 +238,43 @@ function Beranda() {
 
             <div
               ref={scrollRef2}
-              className="grid grid-cols-2 lg:flex lg:flex-row gap-3 mb-4 lg:mb-0 lg:gap-12 py-4 lg:overflow-x-scroll lg:scrollbar-none lg:scroll-smooth mx-4 max-w-full lg:max-w-[90%]"
+              className="flex flex-col sm:flex-row w-full gap-4 sm:gap-8 py-4 mx-4 sm:w-1/2"
             >
-              {menuMinuman1.map((item, index) => (
+              {daftarMinuman.map((minuman) => (
                 <CardBody
-                  key={index}
-                  className="bg-[#EFF3EA] rounded-xl p-3 shadow-md lg:shadow-lg w-full lg:w-[250px] lg:flex-shrink-0"
+                  key={minuman.id}
+                  className="bg-[#EFF3EA] grid grid-cols-2 sm:flex sm:flex-col p-3 items-center sm:gap-0 gap-3 rounded-lg border-2 border-gray-300 shadow-md lg:shadow-lg w-full"
                 >
-                  <Image
-                    src={item.image}
-                    alt={`menu-${index + 1}`}
-                    className="w-full h-auto rounded-lg"
-                  />
-                  <div className="py-3 space-y-2 lg:space-y-1">
-                    <Typography className="text-lg lg:text-xl text-black">
-                      {item.name}
-                    </Typography>
-                    <Typography className="text-sm text-gray-600">
-                      {item.description}
-                    </Typography>
-                    <Typography className="text-md text-black">
-                      {item.price}
-                    </Typography>
+                  <div className="w-full">
+                    <Image
+                      src={minuman.Gambar_Minuman || fotoMinuman}
+                      alt={`menu-${minuman.id + 1}`}
+                      width={200}
+                      height={200}
+                      className="w-full h-full object-cover rounded-lg"
+                    />
                   </div>
-                  <div className="w-full lg:mt-2 flex justify-center items-center">
-                    <Button className="p-1 lg:py-2 w-full rounded-full tracking-widest bg-[#AA5656] shadow-md hover:shadow-md">
-                      beli
-                    </Button>
+                  <div className="flex flex-col w-full flex-1">
+                    <div className="py-3 space-y-2 lg:space-y-1">
+                      <Typography className="text-lg lg:text-xl font-bold text-black">
+                        {minuman.Nama_Minuman}
+                      </Typography>
+                      <Typography className="text-md text-gray-600">
+                        {minuman.Deskripsi_Minuman}
+                      </Typography>
+                    </div>
+                    <div className="w-full flex-col space-y-2 justify-center items-center mt-auto">
+                      <Typography className="text-lg text-black">
+                        {formatRupiah(minuman.Harga_Minuman)}
+                      </Typography>
+                      <Button className="p-1 lg:py-2 w-full lg:rounded-full tracking-widest bg-[#AA5656] shadow-md hover:shadow-md">
+                        beli
+                      </Button>
+                    </div>
                   </div>
                 </CardBody>
               ))}
             </div>
-
             <Button
               className="bg-black bg-opacity-25 p-2 hidden lg:flex justify-center items-center rounded-full hover:bg-opacity-15 hover:shadow-md hover:scale-105 transform transition-all ease-in-out duration-300"
               onClick={scrollRight2}
@@ -365,8 +282,9 @@ function Beranda() {
               <FaChevronRight className="h-5 w-5 text-black" />
             </Button>
           </div>
+          {/* 3 */}
           <div className="w-full">
-            <Typography className="text-black text-start text-2xl lg:text-[36px] font-semibold ml-3 lg:mb-5 lg:ml-10">
+            <Typography className="text-black text-start text-2xl lg:text-[36px] font-semibold ml-3 mb-3 lg:mb-5 lg:ml-10">
               Minuman (non Coffe)
             </Typography>
           </div>
@@ -380,33 +298,39 @@ function Beranda() {
 
             <div
               ref={scrollRef3}
-              className="grid grid-cols-2 lg:flex lg:flex-row gap-3 lg:gap-12 py-4 lg:overflow-x-scroll lg:scrollbar-none lg:scroll-smooth mx-4 max-w-full lg:max-w-[90%]"
+              className="flex flex-col sm:flex-row w-full gap-4 sm:gap-8 py-4 mx-4 sm:w-1/2"
             >
-              {menuMinuman2.map((item, index) => (
+              {daftarMinuman.map((minuman) => (
                 <CardBody
-                  key={index}
-                  className="bg-[#EFF3EA] rounded-xl p-3 shadow-md lg:shadow-lg w-full lg:w-[250px] lg:flex-shrink-0"
+                  key={minuman.id}
+                  className="bg-[#EFF3EA] grid grid-cols-2 sm:flex sm:flex-col p-3 items-center sm:gap-0 gap-3 rounded-lg border-2 border-gray-300 shadow-md lg:shadow-lg w-full"
                 >
-                  <Image
-                    src={item.image}
-                    alt={`menu-${index + 1}`}
-                    className="w-full h-auto rounded-lg"
-                  />
-                  <div className="py-3 space-y-2 lg:space-y-1">
-                    <Typography className="text-lg lg:text-xl text-black">
-                      {item.name}
-                    </Typography>
-                    <Typography className="text-sm text-gray-600">
-                      {item.description}
-                    </Typography>
-                    <Typography className="text-md text-black">
-                      {item.price}
-                    </Typography>
+                  <div className="w-full">
+                    <Image
+                      src={minuman.Gambar_Minuman || fotoMinuman}
+                      alt={`menu-${minuman.id + 1}`}
+                      width={200}
+                      height={200}
+                      className="w-full h-full object-cover rounded-lg"
+                    />
                   </div>
-                  <div className="w-full lg:mt-2 flex justify-center items-center">
-                    <Button className="p-1 lg:py-2 w-full rounded-full tracking-widest bg-[#AA5656] shadow-md hover:shadow-md">
-                      beli
-                    </Button>
+                  <div className="flex flex-col w-full flex-1">
+                    <div className="py-3 space-y-2 lg:space-y-1">
+                      <Typography className="text-lg lg:text-xl text-black">
+                        {minuman.Nama_Minuman}
+                      </Typography>
+                      <Typography className="text-sm text-gray-600">
+                        {minuman.Deskripsi_Minuman}
+                      </Typography>
+                    </div>
+                    <div className="w-full flex-col space-y-2 justify-center items-center mt-auto">
+                      <Typography className="text-md text-black">
+                        {formatRupiah(minuman.Harga_Minuman)}
+                      </Typography>
+                      <Button className="p-1 lg:py-2 w-full rounded-full tracking-widest bg-[#AA5656] shadow-md hover:shadow-md">
+                        beli
+                      </Button>
+                    </div>
                   </div>
                 </CardBody>
               ))}
