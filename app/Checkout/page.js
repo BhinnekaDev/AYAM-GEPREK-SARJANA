84059056e3c32d84451733026473c4f5eafff20f
Sayyid <sayyid.gibran212@gmail.com>
2025-02-17 04:47:13 +0700

    LOADER HALAMAN

--- app/Checkout/page.js
+++ app/Checkout/page.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 // COMPONENTS
 import Navbar from "@/components/navbar";
 import Konten from "@/app/Checkout/components/konten";
+import Loader from "@/components/loader";
 // IMAGE
 import bgCheckout from "@/assets/img/profil/bgProfil.png";
 import bgMobile from "@/assets/img/profil/mobile/bgProfil.png";
@@ -13,6 +14,15 @@ import useCekPengguna from "@/hooks/Backend/useVerifikasiLogin";
 function Page() {
   const [isMobile, setIsMobile] = useState(false);
   const pengguna = useCekPengguna();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     const checkMobile = () => setIsMobile(window.innerWidth < 640);
@@ -23,26 +33,28 @@ function Page() {
   }, []);
 
   return (
-    <div>
-      {pengguna ? (
-        <div
-          className="overflow-hidden min-h-screen bg-[#FFE893]"
-          style={{
-            backgroundImage: `url(${isMobile ? bgMobile.src : bgCheckout.src})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div>
-            <Navbar />
-          </div>
+    <div
+      className="overflow-hidden min-h-screen bg-[#FFE893]"
+      style={{
+        backgroundImage: `url(${
+          isMobile ? String(bgMobile) : String(bgCheckout)
+        })`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Toaster position="top-right" reverseOrder={false} />
+
+      {isLoading ? (
+        <Loader />
+      ) : pengguna ? (
+        <>
+          <Navbar />
           <div className="my-6 lg:m-0">
             <Konten />
           </div>
-        </div>
-      ) : (
-        <Toaster position="top-right" reverseOrder={false} />
-      )}
+        </>
+      ) : null}
     </div>
   );
 }
